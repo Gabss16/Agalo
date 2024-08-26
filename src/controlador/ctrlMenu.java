d64152f0270aa9b71f305e4ee2c0666082516d93
chaichor <christianpro313@gmail.com>
2024-08-26 11:14:27 -0600

    menu

--- src/controlador/ctrlMenu.java
+++ src/controlador/ctrlMenu.java
@@ -3,11 +3,59 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package controlador;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import vista.Dashboard;
+import vista.menu;
+
+
 
 /**
  *
  * @author lagal
  */
-public class ctrlMenu {
+public class ctrlMenu implements MouseListener{
+    
+    menu vista;
+    
+    public ctrlMenu(menu Vista){
+    this.vista = Vista;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        
+        if (e.getSource()== vista.btnHome) {
+            
+            Dashboard objD = new Dashboard();
+        
+        vista.jPCpntenedor.removeAll();
+        vista.jPCpntenedor.add(objD);
+            
+        }
+        
+        
+        
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        
+    }
     
 }
